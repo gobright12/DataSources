1188cb5cb0e428fd067409d3fd29fa50a5117969
gobright12 <34306129+gobright12@users.noreply.github.com>
2023-01-11 21:53:51 +0900

    adding data controller (#3)
    Co-authored-by: Donghyun Kim <donghyun.kim@phloxcorp.io>

--- Sources/DataSources/CollectionViewAdapter.swift
+++ Sources/DataSources/CollectionViewAdapter.swift
@@ -8,64 +8,59 @@
 
 import UIKit
 
-open class CollectionViewAdapter<Element>: Updating {
-
-  public unowned let collectionView: UICollectionView
-
-  public var target: UICollectionView {
-    return collectionView
-  }
-
-  public init(collectionView: UICollectionView) {
-    self.collectionView = collectionView
-  }
-
-  open func insertItems(at indexPaths: [IndexPath], in context: UpdateContext<Element>) {
-
-    collectionView.insertItems(at: indexPaths)
-  }
-
-  open func deleteItems(at indexPaths: [IndexPath], in context: UpdateContext<Element>) {
-
-    collectionView.deleteItems(at: indexPaths)
-  }
-
-  open func reloadItems(at indexPaths: [IndexPath], in context: UpdateContext<Element>) {
-
-    collectionView.reloadItems(at: indexPaths)
-  }
-
-  open func moveItem(at indexPath: IndexPath, to newIndexPath: IndexPath, in context: UpdateContext<Element>) {
-
-    collectionView.moveItem(at: indexPath, to: newIndexPath)
-  }
-
-  open func performBatch(in context: UpdateContext<Element>, animated: Bool, updates: @escaping () -> Void, completion: @escaping () -> Void) {
-
-    if animated {
-      collectionView.performBatchUpdates({
-        updates()
-      }, completion: { result in
-        completion()
-      })
-    } else {
-
-      CATransaction.begin()
-      CATransaction.setDisableActions(true)
-
-      collectionView.performBatchUpdates({
-        updates()
-      }, completion: { result in
-        CATransaction.commit()
-        completion()
-      })
+open class CollectionViewAdapter: Updating {
+    
+    public unowned let collectionView: UICollectionView
+    
+    public var target: UICollectionView {
+        return collectionView
     }
-
-  }
-
-  open func reload(completion: @escaping () -> Void) {
-
-    collectionView.reloadData()
-    completion()
-  }
+    
+    public init(collectionView: UICollectionView) {
+        self.collectionView = collectionView
+    }
+    
+    open func insertItems(at indexPaths: [IndexPath]) {
+        collectionView.insertItems(at: indexPaths)
+    }
+    
+    open func deleteItems(at indexPaths: [IndexPath]) {
+        collectionView.deleteItems(at: indexPaths)
+    }
+    
+    open func reloadItems(at indexPaths: [IndexPath]) {
+        collectionView.reloadItems(at: indexPaths)
+    }
+    
+    open func moveItem(at indexPath: IndexPath, to newIndexPath: IndexPath) {
+        collectionView.moveItem(at: indexPath, to: newIndexPath)
+    }
+    
+    open func performBatch(animated: Bool, updates: @escaping () -> Void, completion: @escaping () -> Void) {
+        if animated {
+            collectionView.performBatchUpdates({
+                updates()
+            }, completion: { result in
+                completion()
+            })
+        } else {
+            
+            CATransaction.begin()
+            CATransaction.setDisableActions(true)
+            
+            collectionView.performBatchUpdates({
+                updates()
+            }, completion: { result in
+                CATransaction.commit()
+                completion()
+            })
+        }
+        
+    }
+    
+    open func reload(completion: @escaping () -> Void) {
+        
+        collectionView.reloadData()
+        completion()
+    }
 }
